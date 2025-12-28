b28dda34e3291d4c6235b5e2604a958c0293a1bc
jeffbarton <jeffbarton411@gmail.com>
2025-12-28 19:03:03 +0000

    now this

--- src/firebase/service-account.ts
+++ src/firebase/service-account.ts
@@ -2,15 +2,14 @@
 // It is safe to store credentials here.
 
 export const serviceAccount = {
-  // Replace with your actual service account credentials
   "type": "service_account",
   "project_id": "studio-7036773577-c4960",
   "private_key_id": "d04423805727b134d19315d0270a256191a3848b",
-  "private_key": "YOUR_PRIVATE_KEY",
-  "client_email": "firebase-adminsdk-78n8j@studio-7036773577-c4960.iam.gserviceaccount.com",
-  "client_id": "116528274139857904135",
+  "private_key": "PASTE YOUR NEW PRIVATE KEY HERE",
+  "client_email": "firebase-adminsdk-g6b0g@studio-7036773577-c4960.iam.gserviceaccount.com",
+  "client_id": "117865239967139196941",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-78n8j%40studio-7036773577-c4960.iam.gserviceaccount.com"
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v2/metadata/x509/firebase-adminsdk-g6b0g%40studio-7036773577-c4960.iam.gserviceaccount.com"
 };
